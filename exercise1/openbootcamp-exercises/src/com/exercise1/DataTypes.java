1425501d4cd528d37db6941c7753f1e302b44ef8
Juan Andrés Cuevas <juanandrescuevas14@gmail.com>
2022-02-08 14:55:59 -0400

    Ejercicio 1 - Java Basico

---
+++ exercise1/openbootcamp-exercises/src/com/exercise1/DataTypes.java
@@ -0,0 +1,22 @@
+package com.exercise1;
+
+public class DataTypes {
+    public static void main(String[] args) {
+        //Texto
+        String var1 = "Mi nombre es Juan";
+
+        //Numericos
+        int var2 = 20;
+        long var3 = 25264894959L;
+        double var4 = 19.6;
+
+        //Booleano
+        boolean var5 = true;
+
+        System.out.println(var1);
+        System.out.println(var2);
+        System.out.println(var3);
+        System.out.println(var4);
+        System.out.println(var5);
+    }
+}
